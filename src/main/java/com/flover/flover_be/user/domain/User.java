48f5e0df5c46de0c97064f7497f50d9263074a1a
LeeJaeRyun <pinixs3@naver.com>
2026-04-27 21:23:45 +0900

    [feat]: User 엔티티 및 Repository 구현

---
+++ src/main/java/com/flover/flover_be/user/domain/User.java
@@ -0,0 +1,47 @@
+package com.flover.flover_be.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "kakao_id", unique = true, nullable = false)
+    private Long kakaoId;
+
+    private String email;
+    private String nickname;
+
+    @Column(name = "profile_image_url")
+    private String profileImageUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    public static User create(Long kakaoId, String email, String nickname, String profileImageUrl) {
+        User user = new User();
+        user.kakaoId = kakaoId;
+        user.email = email;
+        user.nickname = nickname;
+        user.profileImageUrl = profileImageUrl;
+        return user;
+    }
+
+    public void updateProfile(String nickname, String profileImageUrl) {
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
