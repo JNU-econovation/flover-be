696fbb17dad8451f5b549a5c17b50569824585ba
LeeJaeRyun <pinixs3@naver.com>
2026-04-30 10:44:19 +0900

    [feat]: JWT 인증 필터 및 LoginUserId ArgumentResolver 추가

--- src/main/java/com/flover/flover_be/global/config/WebConfig.java
+++ src/main/java/com/flover/flover_be/global/config/WebConfig.java
@@ -1,11 +1,19 @@
 package com.flover.flover_be.global.config;
 
+import com.flover.flover_be.global.auth.LoginUserIdArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final LoginUserIdArgumentResolver loginUserIdArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserIdArgumentResolver);
     }
 }
