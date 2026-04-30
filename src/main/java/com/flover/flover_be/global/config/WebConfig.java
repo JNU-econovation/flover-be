b6ba50a09b98ca65c61507fe56c38956d59d77c0
LeeJaeRyun <pinixs3@naver.com>
2026-04-30 12:22:21 +0900

    [fix]: ObjectMapper Bean 명시적 등록

--- src/main/java/com/flover/flover_be/global/config/WebConfig.java
+++ src/main/java/com/flover/flover_be/global/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.flover.flover_be.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flover.flover_be.global.auth.LoginUserIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -27,5 +29,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserIdArgumentResolver);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
