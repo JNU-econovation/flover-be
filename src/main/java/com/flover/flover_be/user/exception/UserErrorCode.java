cdf530541d8d8a2872e9f85efcb2e520a9b31691
LeeJaeRyun <pinixs3@naver.com>
2026-05-02 22:26:01 +0900

    [refactor]: 이미지 타입 에러코드 CommonErrorCode로 이동

--- src/main/java/com/flover/flover_be/user/exception/UserErrorCode.java
+++ src/main/java/com/flover/flover_be/user/exception/UserErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-    NICKNAME_ALREADY_USED(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다.");
+    NICKNAME_ALREADY_USED(HttpStatus.CONFLICT, "이미 사용 중인 닉네임입니다."),
+    INVALID_PLOGGING_TIME(HttpStatus.BAD_REQUEST, "플로깅 시간은 양수여야 합니다.");
 
     private final HttpStatus status;
     private final String message;
