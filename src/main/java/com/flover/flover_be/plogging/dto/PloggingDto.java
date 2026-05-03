56ece1231db4c427cf38ba839bb8ba2f8b67ca5d
LeeJaeRyun <pinixs3@naver.com>
2026-05-04 00:55:48 +0900

    [feat]: photoUrls 리스트 요소 공백/null 검증 추가

--- src/main/java/com/flover/flover_be/plogging/dto/PloggingDto.java
+++ src/main/java/com/flover/flover_be/plogging/dto/PloggingDto.java
@@ -4,6 +4,7 @@ import com.flover.flover_be.plogging.domain.PloggingMode;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -29,7 +30,7 @@ public class PloggingDto {
             @NotNull @DecimalMin("-180.0") @DecimalMax("180.0") Double endLongitude,
             @NotNull @Valid List<RoutePointRequest> routePoints,
             String mapImageUrl,
-            @NotNull List<String> photoUrls
+            @NotNull List<@NotBlank String> photoUrls
     ) {}
 
     public record RoutePointRequest(
