6dbc8e821de1a5d1ce13f41a8a49707a0696834d
LeeJaeRyun <pinixs3@naver.com>
2026-05-04 01:57:48 +0900

    [feat]: PloggingSessionRepository 사용자별 최신순 조회 메서드 추가

--- src/main/java/com/flover/flover_be/plogging/repository/PloggingSessionRepository.java
+++ src/main/java/com/flover/flover_be/plogging/repository/PloggingSessionRepository.java
@@ -3,5 +3,9 @@ package com.flover.flover_be.plogging.repository;
 import com.flover.flover_be.plogging.domain.PloggingSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PloggingSessionRepository extends JpaRepository<PloggingSession, Long> {
+
+    List<PloggingSession> findAllByUserIdOrderByStartedAtDesc(Long userId);
 }
