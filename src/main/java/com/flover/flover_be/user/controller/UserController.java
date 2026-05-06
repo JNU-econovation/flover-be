9f4d891a36edeebd48280a2bb9a2a5aaccda8222
LeeJaeRyun <pinixs3@naver.com>
2026-05-06 16:48:23 +0900

    [refactor]: 유저 정보 조회 엔드포인트를 /{userId}에서 /me로 변경

--- src/main/java/com/flover/flover_be/user/controller/UserController.java
+++ src/main/java/com/flover/flover_be/user/controller/UserController.java
@@ -7,11 +7,9 @@ import com.flover.flover_be.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +24,9 @@ public class UserController {
 
     private final UserService userService;
 
-    @Operation(summary = "유저 정보 조회", description = "닉네임, 레벨, 프로필 이미지 URL 반환")
-    @GetMapping("/{userId}")
-    public ResponseEntity<UserDto.UserInfoResponse> getUserInfo(@PathVariable @Positive Long userId) {
+    @Operation(summary = "내 정보 조회", description = "닉네임, 레벨, 프로필 이미지 URL 반환")
+    @GetMapping("/me")
+    public ResponseEntity<UserDto.UserInfoResponse> getMyInfo(@LoginUserId Long userId) {
         return ResponseEntity.ok(userService.findUserInfo(userId));
     }
 
